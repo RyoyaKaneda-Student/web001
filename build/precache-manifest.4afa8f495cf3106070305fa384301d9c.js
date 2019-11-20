c87ab83fe4da20825d5be2d719226b44684d3546
RyoyaKaneda-Student <kaneda@ss.cs.osakafu-u.ac.jp>
2019-11-20 19:39:34 +0900

    comment

---
+++ build/precache-manifest.4afa8f495cf3106070305fa384301d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22713cd38d223c249640068ca197913e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0d299a8375f2af0481",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd68c0d112e6d6a1e356",
+    "url": "/static/js/2.63408274.chunk.js"
+  },
+  {
+    "revision": "ae0d299a8375f2af0481",
+    "url": "/static/js/main.ac80ed07.chunk.js"
+  },
+  {
+    "revision": "2e57f6cbc09fc27fc94e",
+    "url": "/static/js/runtime-main.afc35f32.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
